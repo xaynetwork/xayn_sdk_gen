b1f7fd57d087eb0fb3ff55e037a77e697c9d7c62
Frank Pepermans <frank@igindo.com>
2022-12-08 14:36:28 +0100

    fix windows issue with carriage returns

--- generate.sh
+++ generate.sh
@@ -4,7 +4,6 @@
 
 # fetch the JAR for codegen
 wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.2.1/openapi-generator-cli-6.2.1.jar -O openapi-generator-cli.jar
-
 # reset targets directory
 if [ -d "./targets" ]; then rm -Rf ./targets; fi
 mkdir ./targets
@@ -33,7 +32,6 @@ export TOKEN_USERS="1LfFzl0ziRIvR0EcnTHW559qQuDdlGl16BHx8oNg"
 # JavaScript section
 # --------------------------
 
-## vars
 export JS_WKDIR_DOCUMENTS="./targets/$LANG_JS/document_management"
 export JS_WKDIR_USERS="./targets/$LANG_JS/user_management"
 export JS_TEMPLATE_DOCUMENTS="./.test/$LANG_JS/document_management/test"
@@ -45,7 +43,6 @@ export JS_GEN_PROPS_USERS="projectName=$PROJNAME_USERS,projectVersion=$VERSION,a
 # Java section
 # --------------------------
 
-## vars
 export JAVA_WKDIR_DOCUMENTS="./targets/$LANG_JAVA/document_management"
 export JAVA_WKDIR_USERS="./targets/$LANG_JAVA/user_management"
 export JAVA_TEMPLATE_DOCUMENTS="./.test/$LANG_JAVA/document_management/test"
@@ -53,7 +50,6 @@ export JAVA_TEMPLATE_USERS="./.test/$LANG_JAVA/user_management/test"
 export JAVA_GEN_PROPS_DOCUMENTS="projectName=$PROJNAME_DOCUMENTS,artifactVersion=$VERSION,apiPackage=$PKG_DOCUMENTS,artifactId=$ID_DOCUMENTS,asyncNative=true,serializableModel=true,serializationLibrary=gson,snapshotVersion=$VERSION,groupId=com.xayn,licenseName=$LICENSE_NAME,invokerPackage=$PKG_DOCUMENTS,modelPackage=$PKG_DOCUMENTS.models"
 export JAVA_GEN_PROPS_USERS="projectName=$PROJNAME_USERS,artifactVersion=$VERSION,apiPackage=$PKG_USERS,artifactId=$ID_USERS,asyncNative=true,serializableModel=true,serializationLibrary=gson,snapshotVersion=$VERSION,groupId=com.xayn,licenseName=$LICENSE_NAME,invokerPackage=$PKG_USERS,modelPackage=$PKG_USERS.models"
 
-## run codegen
 function codegen() {
   ./scripts/$1/document_management/generate.sh
   ./scripts/$1/user_management/generate.sh
