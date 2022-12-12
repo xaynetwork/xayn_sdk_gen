c11804100f46c1b72e12ecca29e6ad0816d79a9a
Frank Pepermans <frank@igindo.com>
2022-12-12 15:44:46 +0100

    try with env

--- scripts/javascript/document_management/test.sh
+++ scripts/javascript/document_management/test.sh
@@ -1,16 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 source scripts/javascript/common.sh
 
 cd $JS_WKDIR_DOCUMENTS || exit
 
-npm --endpoint=${{ secrets.ENDPOINT }} --token=${{ secrets.TOKEN_DOCUMENTS }} run test
-
-if [ $? -eq 0 ]
-then
-  export JS_TEST_DOCUMENTS=1
-else
-  export JS_TEST_DOCUMENTS=0
-fi
+npm --endpoint=$ENDPOINT --token=$TOKEN_DOCUMENTS run test
 
 cd - || exit
--- scripts/javascript/user_management/test.sh
+++ scripts/javascript/user_management/test.sh
@@ -1,16 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 source scripts/javascript/common.sh
 
 cd $JS_WKDIR_USERS || exit
 
-npm --endpoint=${{ secrets.ENDPOINT }} --token=${{ secrets.TOKEN_USERS }} run test
-
-if [ $? -eq 0 ]
-then
-  JS_TEST_USERS=1
-else
-  JS_TEST_USERS=0
-fi
+npm --endpoint=$ENDPOINT --token=$TOKEN_USERS run test
 
 cd - || exit
