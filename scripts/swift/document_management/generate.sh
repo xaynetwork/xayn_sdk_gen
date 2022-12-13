0a5651b2e4930f6449f78e37877b490c8c91c721
Simon Joecks <simon.joecks@gmail.com>
2022-12-13 13:12:08 -0500

    remove swift install

--- scripts/swift/document_management/generate.sh
+++ scripts/swift/document_management/generate.sh
@@ -5,3 +5,4 @@ source scripts/swift/common.sh
 
 ## run JavaScript codegen
 java -jar openapi-generator-cli.jar generate -i document_management.yaml -g $GENERATED_LANG -o $WKDIR_DOCUMENTS --additional-properties=$GEN_PROPS_DOCUMENTS $PARAMS
+find $WKDIR_DOCUMENTS -type f -name '*.swift' -exec perl -i -pe 's/import\ MobileCoreServices/import\ FoundationNetworking/g' {} + 
