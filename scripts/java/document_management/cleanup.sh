29e67efcbc42e317748f25db55064edbaf5afad1
Frank Pepermans <frank@igindo.com>
2022-12-15 13:46:05 +0100

    inject test System properties via bash instead

--- scripts/java/document_management/cleanup.sh
+++ scripts/java/document_management/cleanup.sh
@@ -6,5 +6,10 @@ source scripts/java/common.sh
 if [ -d $TEMPLATE_DOCUMENTS ]; then rm -Rf $WKDIR_DOCUMENTS/src/test; fi
 rm -Rf $WKDIR_DOCUMENTS/.travis.yml
 rm -Rf $WKDIR_DOCUMENTS/git_push.sh
-cp -R $TEMPLATE_DOCUMENTS/build.gradle $WKDIR_DOCUMENTS/build.gradle
 cp -R $TEMPLATE_DOCUMENTS $WKDIR_DOCUMENTS/src/test
+
+sed -i '/^test {$/a\
+    // allow -D system properties\
+    systemProperty "endpoint", System.getProperty("endpoint")\
+    systemProperty "token", System.getProperty("token")
+' $WKDIR_DOCUMENTS/build.gradle
--- scripts/java/user_management/cleanup.sh
+++ scripts/java/user_management/cleanup.sh
@@ -6,5 +6,10 @@ source scripts/java/common.sh
 if [ -d $TEMPLATE_USERS ]; then rm -Rf $WKDIR_USERS/src/test; fi
 rm -Rf $WKDIR_USERS/.travis.yml
 rm -Rf $WKDIR_USERS/git_push.sh
-cp -R $TEMPLATE_USERS/build.gradle $WKDIR_USERS/build.gradle
 cp -R $TEMPLATE_USERS $WKDIR_USERS/src/test
+
+sed -i '/^test {$/a\
+    // allow -D system properties\
+    systemProperty "endpoint", System.getProperty("endpoint")\
+    systemProperty "token", System.getProperty("token")
+' $WKDIR_USERS/build.gradle
