b71fdac55dfab91d55f592a4ce1c7a71799d9730
Frank Pepermans <frank@igindo.com>
2022-12-16 08:55:32 +0100

    add generation params

--- scripts/swift/common.sh
+++ scripts/swift/common.sh
@@ -6,9 +6,8 @@ WKDIR_DOCUMENTS="./targets/$LANG/document_management"
 WKDIR_USERS="./targets/$LANG/user_management"
 TEMPLATE_DOCUMENTS="./.test/$LANG/document_management/test"
 TEMPLATE_USERS="./.test/$LANG/user_management/test"
-GEN_PROPS_DOCUMENTS=""
-GEN_PROPS_USERS=""
-#--library=alamofire
+GEN_PROPS_DOCUMENTS="projectName=$PROJNAME_DOCUMENTS,artifactVersion=$VERSION,podAuthors=xayn.com,podDescription=$PROJNAME_DOCUMENTS,podLicense=$LICENSE_NAME,podVersion=$VERSION"
+GEN_PROPS_USERS="projectName=$PROJNAME_USERS,artifactVersion=$VERSION,podAuthors=xayn.com,podDescription=$PROJNAME_USERS,podLicense=$LICENSE_NAME,podVersion=$VERSION"
 PARAMS=""
 
 create_git_repo() {
